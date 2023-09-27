38676d1853a52d53e7f5dd3c5aa80015efe6400b
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2023-09-27 22:14:57 +1000

    Finish T1

--- T1 - Semester Test/SemesterTest/SemesterTest/Program.cs
+++ T1 - Semester Test/SemesterTest/SemesterTest/Program.cs
@@ -6,21 +6,21 @@ namespace SemesterTest
     {
         public static void Main(string[] args)
         {
-            FileSystem f = new FileSystem();
+            FileSystem fileSystem = new FileSystem();
             Folder semesterTest = new Folder("Semester Test");
             semesterTest.Add(new File("socket_server", "py", 1024));
             semesterTest.Add(new File("socket_client", "py", 512));
-            f.Add(semesterTest);
+            fileSystem.Add(semesterTest);
 
             Folder nothing = new Folder("Nothing");
-            f.Add(nothing);
-
+            fileSystem.Add(nothing);
+            
             File wireShark = new File("wiresharkCapture", "pcap", 464);
             File headerJSON = new File("header", "json", 16384);
-            f.Add(wireShark);
-            f.Add(headerJSON);  
+            fileSystem.Add(wireShark);
+            fileSystem.Add(headerJSON);  
 
-            f.PrintContents();
+            fileSystem.PrintContents();
         }
     }
 }
